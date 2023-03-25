81b9c726b8da8baff0c778bf4303afa93237026c
Aarni <dabeastelele@gmail.com>
2023-03-25 09:03:57 +0200

    projektin alustusta

---
+++ tasks.py
@@ -0,0 +1,16 @@
+from invoke import task
+
+@task
+def format(ctx):
+    ctx.run("autopep8 --in-place --recursive src", pty=True)
+    
+def start(ctx):
+    pass
+    
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest", pty=True)
+    
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html", pty=True)
